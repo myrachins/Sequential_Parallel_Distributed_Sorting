95625bf0278a8f2b3e46b24b7daccf3b0e938083
Maxim <myurachinskiy@edu.hse.ru>
2019-06-02 19:05:34 +0300

    Initial commit

---
+++ Sequential_Parallel_Distributed_Sorting_Library/src/main/java/MergeSort.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public final class MergeSort {
+//    public int[] SortArray(int[] array) {
+//        if (array.length <= 1) {
+//            return array;
+//        }
+//        int middle = array.length / 2;
+//        int[] leftPart = SortArray(Arrays.copyOfRange(array, 0, middle));
+//    }
+//
+//    public int[] MergeArrays(int[] firstArray, int[] SecondArray) {
+//
+//    }
+}
